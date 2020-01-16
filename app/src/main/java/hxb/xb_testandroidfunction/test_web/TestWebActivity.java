60671f85525e0b16392814a9ab7216cff5633768
01xinbahe10 <161452681@qq.com>
2020-01-16 18:54:26 +0800

    测试修改html

--- app/src/main/java/hxb/xb_testandroidfunction/test_web/TestWebActivity.java
+++ app/src/main/java/hxb/xb_testandroidfunction/test_web/TestWebActivity.java
@@ -1,18 +1,25 @@
 package hxb.xb_testandroidfunction.test_web;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Environment;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
+
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
+
 import hxb.xb_testandroidfunction.R;
+import hxb.xb_testandroidfunction.test_util.Logger;
 import hxb.xb_testandroidfunction.test_web.web.AdvancedWebView;
 
 /**
@@ -21,7 +28,10 @@ import hxb.xb_testandroidfunction.test_web.web.AdvancedWebView;
  */
 
 public class TestWebActivity extends FragmentActivity implements AdvancedWebView.Listener{
+    private static final String TAG = "TestWebActivity";
+    private static final Logger LOGGER = new Logger(TAG,"");
     private AdvancedWebView mWebView;
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,12 +44,28 @@ public class TestWebActivity extends FragmentActivity implements AdvancedWebView
         mWebView.setCookiesEnabled(true);
         mWebView.setThirdPartyCookiesEnabled(true);
 
+        WebSettings webSettings = mWebView.getSettings();
+        //设置启动概述模式浏览界面、使用宽屏的视图窗口。
+        /*webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);*/
+        //允许调用Js的权限
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+
         mWebView.setWebViewClient(new WebViewClient(){
 
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
                 Toast.makeText(TestWebActivity.this, "Finished loading", Toast.LENGTH_SHORT).show();
+                String info2 = "[{\"NAME\": \"未完成\", \"VALUE\":36 }, {\"NAME\": \"已完成\", \"VALUE\":25 }, {\"NAME\": \"超时完成\", \"VALUE\":25 }, {\"NAME\": \"已取消\", \"VALUE\":25 } ]";
+                mWebView.loadUrl("javascript:workplan('" + info2 + "')");
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+
+                return true;
             }
         });
 
@@ -53,11 +79,9 @@ public class TestWebActivity extends FragmentActivity implements AdvancedWebView
         });
 
 //        mWebView.loadUrl();
-        String info1 = "[{\"NAME\": \"未完成\", \"VALUE\": 12 }, {\"NAME\": \"已完成\", \"VALUE\": 30 }, {\"NAME\": \"超时完成\", \"VALUE\": 15 }, {\"NAME\": \"已取消\", \"VALUE\": 43 } ]";
         if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
             mWebView.getSettings().setAllowFileAccess(true);
-//            mWebView.loadUrl("file:///android_asset/statistics/workplan.html");
-            mWebView.loadUrl("file:///android_asset/statistics/workplan.html:javascript:workplan('" + info1 + "')");
+            mWebView.loadUrl("file:///android_asset/statistics/workplan.html");
         }
     }
 
@@ -106,16 +130,19 @@ public class TestWebActivity extends FragmentActivity implements AdvancedWebView
 
     @Override
     public void onPageError(int errorCode, String description, String failingUrl) {
-        Toast.makeText(TestWebActivity.this, "onPageError(errorCode = "+errorCode+",  description = "+description+",  failingUrl = "+failingUrl+")", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(TestWebActivity.this, "onPageError(errorCode = "+errorCode+",  description = "+description+",  failingUrl = "+failingUrl+")", Toast.LENGTH_SHORT).show();
+        LOGGER.e( "onPageError: errorCode = "+errorCode+"  , description = "+description+"  , failingUrl = "+failingUrl );
     }
 
     @Override
     public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength, String contentDisposition, String userAgent) {
-        Toast.makeText(TestWebActivity.this, "onDownloadRequested(url = "+url+",  suggestedFilename = "+suggestedFilename+",  mimeType = "+mimeType+",  contentLength = "+contentLength+",  contentDisposition = "+contentDisposition+",  userAgent = "+userAgent+")", Toast.LENGTH_LONG).show();
+//        Toast.makeText(TestWebActivity.this, "onDownloadRequested(url = "+url+",  suggestedFilename = "+suggestedFilename+",  mimeType = "+mimeType+",  contentLength = "+contentLength+",  contentDisposition = "+contentDisposition+",  userAgent = "+userAgent+")", Toast.LENGTH_LONG).show();
+        LOGGER.e("onDownloadRequested: url = "+url+"  , suggestedFilename = "+suggestedFilename+"  ,  mimeType = "+mimeType+"  ,  contentLength = "+contentLength+"   ,  contentDisposition = "+contentDisposition+"  ,  userAgent = "+userAgent);
     }
 
     @Override
     public void onExternalPageRequest(String url) {
-        Toast.makeText(TestWebActivity.this, "onExternalPageRequest(url = "+url+")", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(TestWebActivity.this, "onExternalPageRequest(url = "+url+")", Toast.LENGTH_SHORT).show();
+        LOGGER.e("onExternalPageRequest(url = "+url+")");
     }
 }
