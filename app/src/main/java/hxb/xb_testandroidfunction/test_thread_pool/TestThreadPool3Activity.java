35305860a2c7bc57564f33f6e5d61430509b6b94
01xinbahe10 <161452681@qq.com>
2020-01-18 13:46:34 +0800

    测试 ThreadLocal 的作用

--- app/src/main/java/hxb/xb_testandroidfunction/test_thread_pool/TestThreadPool3Activity.java
+++ app/src/main/java/hxb/xb_testandroidfunction/test_thread_pool/TestThreadPool3Activity.java
@@ -24,6 +24,12 @@ public class TestThreadPool3Activity extends FragmentActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_thread_pool3);
+//       testThreadPriority();
+        testThreadLocal();
+    }
+
+    //测试线程执行的优先级
+    private void testThreadPriority(){
         ExecutorService executorService = new PriorityExecutor(5, false);
         for (int i = 0; i < 20; i++) {
             PriorityRunnable priorityRunnable = new PriorityRunnable(PriorityRunnable.Priority.NORMAL, new Runnable() {
@@ -65,4 +71,55 @@ public class TestThreadPool3Activity extends FragmentActivity{
             executorService.execute(priorityRunnable);
         }
     }
+
+
+    //测试ThreadLocal在各个线程中使用的情况
+    private void testThreadLocal(){
+        ExecutorService executorService = new PriorityExecutor(5, false);
+        for (int i = 0; i < 5; i++) {
+
+            int finalI = i;
+            executorService.execute(new PriorityRunnable(PriorityRunnable.Priority.NORMAL, new Runnable() {
+                @Override
+                public void run() {
+                    switch (finalI){
+                        case 0://线程一，以下依次类推
+                            updateStaticThreadLocal(0);
+                            break;
+                        case 1:
+                            updateStaticThreadLocal(1);
+                            break;
+                        case 2:
+                            updateStaticThreadLocal(2);
+                            break;
+                        case 3:
+                            updateStaticThreadLocal(3);
+                            break;
+                        case 4:
+                            updateStaticThreadLocal(4);
+                            break;
+                    }
+                }
+            }));
+        }
+
+    }
+
+
+    //测试静态状态下的ThreadLocal各个线程的值变化
+    public static void updateStaticThreadLocal(int testIndex){
+        Log.e("TAG", "updateStaticThreadLocal11:  "+threadLocal.get() );
+        threadLocal.set(testIndex);
+        Log.e("TAG", "updateStaticThreadLocal22:  "+threadLocal.get() );
+    }
+    /*
+    * ThreadLocal是为了保证每个线程中的数据是相互隔离的，互不干扰
+    * */
+    private static ThreadLocal<Integer> threadLocal = new ThreadLocal<Integer>(){
+        @Nullable
+        @Override
+        protected Integer initialValue() {
+            return 1;
+        }
+    };
 }
