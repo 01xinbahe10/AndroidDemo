cba6dd802de9ed1e371ed0799dadd6a66b81d93c
01xinbahe10 <161452681@qq.com>
2020-03-18 09:11:48 +0800

    Tv View同层级与跨层级的焦点移位

--- app/src/main/java/hxb/xb_testandroidfunction/test_util/DataUtil.java
+++ app/src/main/java/hxb/xb_testandroidfunction/test_util/DataUtil.java
@@ -104,4 +104,63 @@ public class DataUtil {
         System.arraycopy(twoArray, 0, newArray, firstLength, twoLength);
         return newArray;
     }
+
+
+    /*****
+     * 字符串匹配（2020-03-18未验证）
+     * Java代码实现KMP模式匹配
+     *
+     * @param stringS 主串S
+     * @param stringT 模式串T
+     */
+    public static boolean match(String stringS, String stringT) {
+        char[] charsS = stringS.toCharArray();
+        char[] charsT = stringT.toCharArray();
+        int[] next = getNextPlus(stringT);
+        int j = 0;
+        for (int i = 0, sizeI = charsS.length; i < sizeI; i ++) {
+            // 此处0 > j其实就是针对next[j]=-1的情况，跳过主串当前字符的对比
+            if (0 > j || charsT[j] == charsS[i]) {
+                j ++;
+            } else {
+                j = next[j];
+                i --; // 实现继续对比主串的同一个位置的字符
+            }
+            if (charsT.length == j) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Java代码实现KMP模式匹配算法next[]推导过程
+     * 针对next[]推荐对比字符相同问题优化
+     *
+     * @param stringT 模式串
+     * */
+    private static int[] getNextPlus(String stringT) {
+        char[] chars = stringT.toCharArray();
+        int[] next = new int[chars.length];
+        next[0] = -1; // 这是一个必然的结果，不管是对什么模式串，以此为突破点往后推导
+        for (int i = 1; i < chars.length; i ++) {
+            int j = next[i - 1];
+            while (true) {
+                if (-1 == j || chars[i - 1] == chars[j]) {
+                    next[i] = j + 1;
+                    break;
+                } else {
+                    j = next[j];
+                }
+            }
+        }
+
+        // 优化版增添代码
+        for (int i = 0; i < next.length; i ++) {
+            if (0 <= next[i] && chars[next[i]] == chars[i]) {
+                next[i] = next[next[i]];
+            }
+        }
+        return next;
+    }
 }
