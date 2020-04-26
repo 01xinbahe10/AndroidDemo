50c405c13222fefd76671107f27fc0de052c03ef
01xinbahe10 <161452681@qq.com>
2020-04-26 10:27:35 +0800

    测试Scheme

--- app/src/main/java/hxb/xb_testandroidfunction/test_scheme/TestSchemeActivity.java
+++ app/src/main/java/hxb/xb_testandroidfunction/test_scheme/TestSchemeActivity.java
@@ -2,6 +2,7 @@ package hxb.xb_testandroidfunction.test_scheme;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -37,10 +38,10 @@ public class TestSchemeActivity extends FragmentActivity{
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //APP内部跳转
-//                Uri uri0 = Uri.parse("xb://test_function:1/test_scheme");
-//                Intent intent = new Intent(Intent.ACTION_VIEW,uri0 );
-//                startActivity(intent);
+                //APP内部/外部跳转（如果有两个以上的app是相同的Uri,系统会跳出弹窗让用户选择）
+                Uri uri0 = Uri.parse("xb://test_function:1/test_scheme");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri0 );
+                startActivity(intent);
 
                 //唤起其它APP(TestProject)中的TestSchemaActivity
 //                Uri uri1 = Uri.parse("xb://test_project:1/test");
@@ -48,9 +49,9 @@ public class TestSchemeActivity extends FragmentActivity{
 //                startActivity(intent1);
 
                 //唤起其它APP并指定界面
-                Intent intent2 = new Intent();
-                intent2.setClassName("com.han.testproject","com.han.testproject.MainActivity");
-                startActivity(intent2);
+               /* Intent intent2 = new Intent();
+                intent2.setClassName("com.example.testjpeg","com.example.testjpeg.SchemeAct");
+                startActivity(intent2);*/
             }
         });
     }
---
+++ testjpeg/src/main/java/com/example/testjpeg/SchemeAct.java
@@ -0,0 +1,17 @@
+package com.example.testjpeg;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by hxb on  2020/4/26
+ */
+public class SchemeAct extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_scheme);
+    }
+}
