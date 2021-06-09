d3a896ddd90495ac4576aa0f7489fb32eede6918
01xinbahe10 <1614526981@qq.com>
2021-06-09 11:27:18 +0800

    更新配置

--- aac/src/main/java/com/example/aac/test/protocase/TestProtoFt.java
+++ aac/src/main/java/com/example/aac/test/protocase/TestProtoFt.java
@@ -1,11 +1,21 @@
 package com.example.aac.test.protocase;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Lifecycle;
 
 import com.example.aac.R;
 import com.example.aac.base_frame.BaseFragment;
@@ -17,6 +27,8 @@ import proto.data.ProtoCase;
 
 public class TestProtoFt extends BaseFragment<FtTestProtoBinding, BaseViewModel> {
     private final String TAG = "XB_"+TestProtoFt.class.getName();
+
+    private ViewTreeObserver.OnWindowFocusChangeListener windowFocusChangeListener;
     @Override
     protected int initContentView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return R.layout.ft_test_proto;
@@ -26,6 +38,8 @@ public class TestProtoFt extends BaseFragment<FtTestProtoBinding, BaseViewModel>
     protected int initViewModelId() {
         return 0;
     }
+
+
 
     @Override
     public void initViewObservable() {
@@ -48,5 +62,79 @@ public class TestProtoFt extends BaseFragment<FtTestProtoBinding, BaseViewModel>
         }
 
         viewDataBinding.tvText.setText(person.toString());
+
+        viewDataBinding.tvText.setOnClickListener(v -> {
+            Log.e(TAG, "initViewObservable: >>>>>>>>>>>>>>>>>> " );
+               new AlertDialog.Builder(getContext())
+                       .setTitle("测试")
+                       .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                           @Override
+                           public void onCancel(DialogInterface dialog) {
+                               dialog.dismiss();
+                           }
+                       })
+                       .create().show();
+        });
+        InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT,0);
+        imm.showSoftInput(viewDataBinding.getRoot(),0);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            windowFocusChangeListener = hasFocus -> {
+
+                Log.e(TAG, "initViewObservable: >>>>>>>>>  "+hasFocus+"    "+(getLifecycle().getCurrentState()== Lifecycle.State.RESUMED));
+//                if (imm.isActive()) { //如果开启
+//                imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, InputMethodManager.HIDE_NOT_ALWAYS); //关闭软键盘，开启方法相同
+//
+//                    imm.showSoftInput(viewDataBinding.getRoot(),0);//显示键盘
+//                    imm.hideSoftInputFromWindow(viewDataBinding.getRoot().getWindowToken(),0);//隐藏键盘
+//                }
+
+//                if (hasFocus && !imm.isActive() || !hasFocus && imm.isActive()){
+//                    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,InputMethodManager.HIDE_NOT_ALWAYS);
+//                }
+
+//                if (getView().hasFocusable()){
+//
+//                }
+//                if(getView().hasFocus()){
+//
+//                }
+//                getView().setFocusable(true);
+//                getView().setFocusableInTouchMode(true);
+//                getView().requestFocus();
+
+            };
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            viewDataBinding.getRoot().getViewTreeObserver().addOnWindowFocusChangeListener(windowFocusChangeListener);
+        }
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause: " );
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop: " );
+    }
+
+    @Override
+    public void onDestroyView() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            viewDataBinding.getRoot().getViewTreeObserver().removeOnWindowFocusChangeListener(windowFocusChangeListener);
+        }
+        super.onDestroyView();
     }
 }
